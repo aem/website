09f402be80406f826a10373317ccf165e5f6e467
aem <amarkon895@gmail.com>
2016-09-28 11:45:48 -0400

    minor meta updates

--- code/client/containers/ContactAdam.js
+++ code/client/containers/ContactAdam.js
@@ -5,7 +5,7 @@ export default class Home extends Component {
   render() {
     return (
       <section className="container-fluid">
-        {title("Contact Adam Markon")}
+        {title("Contact Me | Adam Markon")}
         <h2 className="underline">Contact Adam</h2>
         <blockquote>
           Contact form here
