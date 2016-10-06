75e2bfdac7d4b086c3d8567f2156f55885ec2b80
aem <amarkon895@gmail.com>
2016-10-06 15:06:50 -0400

    fix typo in education

--- code/client/Constants.js
+++ code/client/Constants.js
@@ -3,7 +3,7 @@ import { List, Map } from 'immutable';
 const northeasternEducation = new List([
   new Map({left: 'Northeastern University', right: 'September 2013 – Present'}),
   new Map({left: 'College of Computer and Information Science', right: 'Boston, MA'}),
-  new Map({left: 'Canddidate for Bachelor of Science in Computer Science and Business Administration, 2018', right: ''})
+  new Map({left: 'Candidate for Bachelor of Science in Computer Science and Business Administration, 2018', right: ''})
 ]);
 
 const eaganEducation = new List([
